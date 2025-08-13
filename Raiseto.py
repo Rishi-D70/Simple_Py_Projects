3d10b15fcb94f9a92a9fc4997bf05249841b3054
Rishi-D70 <rishidulhani7070@gmail.com>
2025-08-13 12:46:06 +0530

    Remove all files from repository

--- Raiseto.py
+++
@@ -1,9 +0,0 @@
-base_num = int(input("Enter your base number: "))
-power_num = int(input("Enter your rasieto (^) number: "))
-
-def raise_to_function(base_num, power_num):
-    result = 1
-    for num in range(power_num):
-        result = result * base_num
-    return result
-print(raise_to_function(int(base_num), int(power_num)))
